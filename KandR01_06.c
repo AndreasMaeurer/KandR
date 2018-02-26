701edc07cb80312627d49f9562f2956f372f18f0
AndreasMaeurer <amaeurer@miners.utep.edu>
2018-02-25 21:31:32 -0700

    firt commit

---
+++ KandR01_06.c
@@ -0,0 +1,15 @@
+/*
+ * Exercsise 1-6. Verify that the expression getchar() != EOF is 0 or 1. 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    a = EOF;
+    
+    printf("%d\n", a);
+    
+    //So the point of this excersise I guess is to show that EOF equals -1...
+}
